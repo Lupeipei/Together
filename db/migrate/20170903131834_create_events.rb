019b96ca8681b911eae7a75c4c329405d1b236e8
Lucia <learningleadtorebirth@gmail.com>
2017-09-03 22:07:49 +0800

    create events model

---
+++ db/migrate/20170903131834_create_events.rb
@@ -0,0 +1,14 @@
+class CreateEvents < ActiveRecord::Migration[5.0]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.text :description
+      t.string :address
+      t.string :sponsor
+      t.integer :limited_num
+      t.datetime :start_time
+      t.datetime :end_time
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823062109) do
+ActiveRecord::Schema.define(version: 20170903131834) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "address"
+    t.string   "sponsor"
+    t.integer  "limited_num"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
