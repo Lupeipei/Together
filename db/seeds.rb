53f50f29975491a1522514f0e4bd4827d4f2bdf5
Lucia <learningleadtorebirth@gmail.com>
2017-09-06 09:55:52 +0800

    use ipic to upload pics

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,14 +47,16 @@ Event.create!(
   category_id: 1,
   user_id: users.sample.id,
   title: "空体丨周五晚独立音乐分享会",
-  description: "如果要把世界填充满的话，用音乐和食物，就够了。鼓手，吉他手，键盘手，民谣歌手，原创音乐人……来这里，听他们的梦想与坚持。",
+  description: "如果要把世界填充满的话，用音乐和食物，就够了。<br>
+  鼓手，吉他手，键盘手，民谣歌手，原创音乐人……<br>
+  来这里，听他们的梦想与坚持。",
   address: "广东深圳空体新媒体实验室虚拟大学园R3-A栋1楼",
   sponsor: "空体",
   limited_num: 300,
   start_time: Time.local(2017, 10, 25, 19, 00),
   end_time: Time.local(2017, 10, 25, 22, 00) ,
   status: ["preparing","processing"].sample,
-  logo: MiniMagick::Image.open("#{Rails.root}/app/assets/images/music/102.jpg")
+  logo: MiniMagick::Image.open("https://ws2.sinaimg.cn/large/006tNc79gy1fj9lfh5cjnj31kw16o1kx.jpg")
 )
 
 
@@ -63,14 +65,17 @@ Event.create!(
   category_id: 1,
   user_id: users.sample.id,
   title: "广州追梦#ukulele#周末免费公开课",
-  description: "【只要2小时】从零基础到自弹自唱—达成， 认识更多有趣的朋友， 感受尤克里里的独特魅力， 收获更多不一样的欢乐体验。",
+  description: "【只要2小时】从零基础到自弹自唱—达成， <br>
+                认识更多有趣的朋友， <br>
+                感受尤克里里的独特魅力， <br>
+                收获更多不一样的欢乐体验。",
   address: "广东广州海珠区昌岗中路211-2号信和苑2栋1502",
   sponsor: "追梦音乐工作室",
   limited_num: 300,
   start_time: Time.local(2017, 10, 15, 19, 00),
   end_time: Time.local(2017, 10, 15, 22, 00) ,
   status: ["preparing","processing"].sample,
-  logo: MiniMagick::Image.open("#{Rails.root}/app/assets/images/music/201.jpg")
+  logo: MiniMagick::Image.open("https://ws2.sinaimg.cn/large/006tNc79gy1fj9lfghma5j30rs0kugoz.jpg")
 )
 
 # 户外2
@@ -79,8 +84,8 @@ Event.create!(
   category_id: 2,
   user_id: users.sample.id,
   title: "重逢箭扣长城，东线穿越",
-  description: "活动线路：渔场—箭扣—小布达拉—正北楼—牛角边—慕田峪—营北沟；
-                活动强度：全程8KM左右，上升下降均约900米，
+  description: "活动线路：渔场—箭扣—小布达拉—正北楼—牛角边—慕田峪—营北沟；<br>
+                活动强度：全程8KM左右，上升下降均约900米，<br>
                 集合时间：惠新西街北口B口，早7点20分",
   address: "北京怀柔箭扣长城",
   sponsor: "东岸户外",
@@ -88,7 +93,7 @@ Event.create!(
   start_time: Time.local(2017, 11, 15, 07, 00),
   end_time: Time.local(2017, 11, 15, 12, 00) ,
   status: ["preparing","processing"].sample,
-  logo: MiniMagick::Image.open("#{Rails.root}/app/assets/images/travel/101.jpg")
+  logo: MiniMagick::Image.open("https://ws2.sinaimg.cn/large/006tNc79gy1fj9lhd50vqj30ku0dwgpb.jpg")
 )
 
 
@@ -104,7 +109,7 @@ Event.create!(
   start_time: Time.local(2017, 10, 15, 19, 00),
   end_time: Time.local(2017, 10, 15, 22, 00) ,
   status: ["preparing","processing"].sample,
-  logo: MiniMagick::Image.open("#{Rails.root}/app/assets/images/travel/201.jpg")
+  logo: MiniMagick::Image.open("https://ws1.sinaimg.cn/large/006tNc79gy1fj9lhciprpj30rs0g9q4f.jpg")
 )
 
 # 约厨3
@@ -114,11 +119,11 @@ Event.create!(
   category_id: 3,
   user_id: users.sample.id,
   title: "泡面食堂大侠召集令",
-  description: "泡面食堂大侠召集令！邀请你来亲尝为你收刮来的世界级的泡面！
-                让你来评价一下，哪一款泡面成为你的freestyel！
-                你可以是很爱吃泡面的吃货
-                游侠客帮你架起与泡面的友谊之桥
-                你也可以是很爱凑热闹的XX群众
+  description: "泡面食堂大侠召集令！邀请你来亲尝为你收刮来的世界级的泡面！<br>
+                让你来评价一下，哪一款泡面成为你的freestyel！<br>
+                你可以是很爱吃泡面的吃货<br>
+                游侠客帮你架起与泡面的友谊之桥<br>
+                你也可以是很爱凑热闹的XX群众<br>
                 游侠客【泡面食堂】最不缺的就是热闹！",
   address: "浙江杭州游侠客民宿",
   sponsor: "游侠客",
@@ -126,7 +131,7 @@ Event.create!(
   start_time: Time.local(2017, 10, 10, 19, 00),
   end_time: Time.local(2017, 10, 10, 22, 00) ,
   status: ["preparing","processing"].sample,
-  logo: MiniMagick::Image.open("#{Rails.root}/app/assets/images/cooking/noddles.jpg")
+  logo: MiniMagick::Image.open("https://ws3.sinaimg.cn/large/006tNc79gy1fj9li8mjq7j30rs0fn13q.jpg")
 )
 
 Event.create!(
@@ -134,8 +139,8 @@ Event.create!(
   category_id: 3,
   user_id: users.sample.id,
   title: "心灵厨房",
-  description: "唯美食与爱不可辜负,
-                而一饭一蔬，皆是爱意,
+  description: "唯美食与爱不可辜负,<br>
+                而一饭一蔬，皆是爱意,<br>
                 所以，这里是一个关于食物和心理学的课程",
   address: "陕西西安碑林区长安北路皇族雅苑别墅",
   sponsor: "卷毛银时",
@@ -143,7 +148,7 @@ Event.create!(
   start_time: Time.local(2017, 11, 15, 10, 00),
   end_time: Time.local(2017, 11, 15, 12, 00) ,
   status: ["preparing","processing"].sample,
-  logo: MiniMagick::Image.open("#{Rails.root}/app/assets/images/cooking/102.jpg")
+  logo: MiniMagick::Image.open("https://ws4.sinaimg.cn/large/006tNc79gy1fj9li8zya9j30k00d9407.jpg")
 )
 
 # 交友4
@@ -153,14 +158,18 @@ Event.create!(
   category_id: 4,
   user_id: users.sample.id,
   title: "【无意义大赛】全宇宙最无聊的比赛，太扯淡了!",
-  description: "赛事介绍：1、捏泡泡大赛：捏泡泡才能满足的叫手感； 2、给陌生人打电话：谁聊最长谁赢啊，3、吐豆子大赛：听说射程好的都在这……关于这样的扯淡游戏，我们还准备了十几个，想不想来嘛？",
+  description: "赛事介绍：<br>
+  1、捏泡泡大赛：捏泡泡才能满足的叫手感；<br>
+   2、给陌生人打电话：谁聊最长谁赢啊; <br>
+   3、吐豆子大赛：听说射程好的都在这……<br>
+   关于这样的扯淡游戏，我们还准备了十几个，想不想来嘛？",
   address: "浙江杭州运河大兜路146号游侠客民宿",
   sponsor: "游侠客",
   limited_num: 60,
   start_time: Time.local(2017, 10, 15, 19, 00),
   end_time: Time.local(2017, 10, 15, 22, 00) ,
   status: ["preparing","processing"].sample,
-  logo: MiniMagick::Image.open("#{Rails.root}/app/assets/images/friends/101.png")
+  logo: MiniMagick::Image.open("https://ws3.sinaimg.cn/large/006tNc79gy1fj9ljmpheqj30f108648v.jpg")
 )
 
 Event.create!(
@@ -168,15 +177,15 @@ Event.create!(
   category_id: 4,
   user_id: users.sample.id,
   title: "第二届ACG国际艺术展：展会作品大招募",
-  description: "我们不仅是一个静态艺术展,
-                更是一个贩卖视觉艺术的超级市场,
-                我们不仅是一个创意市集,
-                更是一个视觉艺术大party,
-                我们不仅有来自国外的艺术表演节目,
-                还有来自名校优秀学生的服装秀,
-                我们不仅有国内外大师云集的艺术论坛交流,
-                还有集万千糕点、酒水于一身的交流酒会,
-                欢迎各种视觉噪音,
+  description: "我们不仅是一个静态艺术展<br>
+                更是一个贩卖视觉艺术的超级市场<br>
+                我们不仅是一个创意市集<br>
+                更是一个视觉艺术大party<br>
+                我们不仅有来自国外的艺术表演节目<br>
+                还有来自名校优秀学生的服装秀<br>
+                我们不仅有国内外大师云集的艺术论坛交流<br>
+                还有集万千糕点、酒水于一身的交流酒会<br>
+                欢迎各种视觉噪音<br>
                 让视觉也躁起来",
   address: "深圳南山大冲国际中心5号楼6楼CD单元",
   sponsor: "ACG国际艺术教育",
@@ -184,7 +193,7 @@ Event.create!(
   start_time: Time.local(2017, 12, 15, 19, 00),
   end_time: Time.local(2017, 12, 15, 20, 00) ,
   status: ["preparing","processing"].sample,
-  logo: MiniMagick::Image.open("#{Rails.root}/app/assets/images/friends/201.jpg")
+  logo: MiniMagick::Image.open("https://ws2.sinaimg.cn/large/006tNc79gy1fj9lvg6cifj30xc0m8n37.jpg")
 )
 
 # 读书5
@@ -194,10 +203,10 @@ Event.create!(
   category_id: 5,
   user_id: users.sample.id,
   title: "光阴读书会 | 朗读场",
-  description: "朗读，是人与生俱来的能力。无论乡关何处，情相通。
-                朗读，是古已有之的音乐。快慢行柔、抑扬顿挫，皆成乐章。
-                关闭其他感官，耳朵借我，感受声波经空气带来的按摩。
-                放下一切评判，把心打开，体会平凡文字的撩拨。
+  description: "朗读，是人与生俱来的能力。无论乡关何处，情相通。<br>
+                朗读，是古已有之的音乐。快慢行柔、抑扬顿挫，皆成乐章。<br>
+                关闭其他感官，耳朵借我，感受声波经空气带来的按摩。<br>
+                放下一切评判，把心打开，体会平凡文字的撩拨。<br>
                 带上喜欢的文字和你的故事，沉浸在9月的光阴里、情感的交融中。",
   address: "上海静安芷江西路151号 文化中心 4楼多功能厅",
   sponsor: "书虫部落",
@@ -205,7 +214,7 @@ Event.create!(
   start_time: Time.local(2017, 11, 11, 19, 00),
   end_time: Time.local(2017, 11, 11, 21, 00) ,
   status: ["preparing","processing"].sample,
-  logo: MiniMagick::Image.open("#{Rails.root}/app/assets/images/reading/101.jpg")
+  logo: MiniMagick::Image.open("https://ws1.sinaimg.cn/large/006tNc79ly1fj9lvxqgqej31kw0whtfy.jpg")
 )
 
 # 游戏6
@@ -215,14 +224,15 @@ Event.create!(
   category_id: 6,
   user_id: users.sample.id,
   title: "周末拒绝宅，一起狼人杀吧",
-  description: "一局狼人杀会让你爱上桌游, 也会使你深刻体会快乐真的会让时间变得匆匆,加入我们，一起玩耍吧",
+  description: "一局狼人杀会让你爱上桌游, 也会使你深刻体会快乐真的会让时间变得匆匆<br>
+  加入我们，一起玩耍吧",
   address: "北京海淀区避风塘",
   sponsor: "殽赦",
   limited_num: 15,
   start_time: Time.local(2017, 10, 10, 14, 00),
   end_time: Time.local(2017, 10, 10, 18, 00) ,
   status: ["preparing","processing"].sample,
-  logo: MiniMagick::Image.open("#{Rails.root}/app/assets/images/game/601.jpeg")
+  logo: MiniMagick::Image.open("https://ws4.sinaimg.cn/large/006tNc79ly1fj9lweuvsfj30fe083gmf.jpg")
 )
 
 puts "Events are created"
