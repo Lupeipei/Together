afedff26b5a8e0a0e757acf93236f13092eef8c5
Lucia <learningleadtorebirth@gmail.com>
2017-09-09 22:22:56 +0800

    update qiniu up host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,5 +6,5 @@ CarrierWave.configure do |config|
   config.qiniu_bucket_domain  = ENV["ovyx8s2zv.bkt.clouddn.com"]
   config.qiniu_block_size     = 4*1024*1024
   config.qiniu_protocol       = "http"
-  config.qiniu_up_host        = "http://up.qiniu.com"
+  config.qiniu_up_host        = "http://up-z2.qiniu.com"
 end
