cea389e366a8fc323db0e520445e913ab9de3884
Lucia <learningleadtorebirth@gmail.com>
2017-09-15 11:46:46 +0800

    use ajax for reivew delete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,18 +11,15 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.event = @event
     @review.user = current_user
-    if @review.save
-      redirect_to event_path(@event), notice: "评论成功"
-    else
-      redirect_to event_path(@event), alert: "不能为空"
-    end
+    @review.save
+    # redirect_to event_path(@event), notice: "评论成功"
   end
 
   def destroy
     @event = Event.find(params[:event_id])
     @review = Review.find(params[:id])
     @review.destroy
-    redirect_to :back, alert: "评论已经删除"
+    # redirect_to :back, alert: "评论已经删除"
   end
 
   private
